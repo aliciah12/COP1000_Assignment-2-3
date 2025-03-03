cbb04509a294e3d68950a5049fbc8b88d2588724
aliciah12 <ahare3@hawkmail.hccfl.edu>
2025-03-02 17:14:56 -0800

    Add files via upload

---
+++ COP1000Assignment 2-3.py
@@ -0,0 +1,14 @@
+# furniure.py
+retailPrice = 325
+wholesalePrice = 200
+profit = retailPrice - wholesalePrice
+salePrice = retailPrice * 0.75
+saleProfit = salePrice - wholesalePrice
+print("Retail Price:", retailPrice)
+print("Wholesale Price:", wholesalePrice)
+print("Profit:", profit)
+print("Sale Price:", salePrice)
+print("Sale Profit:", saleProfit)
+
+ 
+ 
